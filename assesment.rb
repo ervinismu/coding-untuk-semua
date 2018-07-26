8c2fb4fe4d7493b775a5852093eb6f96a15abc9e
ervinismu <social.ervin@gmail.com>
2018-07-26 09:58:03 +0700

    [erpin] add file assesment

---
+++ assesment.rb
@@ -0,0 +1,14 @@
+-	Buat contoh variable dengan minimal 3 type data
+- Buat contoh looping minimal 2
+-	Buat conditional statement menggunakkan if/elsif minimal 2 contoh
+-	Buat 2 method, single line dan multi line block
+-	Buat contoh iterasi dengan perhitungan value nya, (+, - , /, %, *)
+
+#
+# masing masing kodenya dijelasin menggunakkan komentar pakai (#)
+#
+- clone
+- buat branch namanya (feature/nama)
+- taruh di folder dengan nama masing^2
+- di push ke branch
+- terakhir, Merge Request diassign ke saya
