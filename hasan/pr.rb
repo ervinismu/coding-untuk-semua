8cb48c7b48ce7dca659a11da5f1c666bd2b3ef89
Tasdiq Hasan <hasantdq@gmail.com>
2018-07-26 20:22:33 +0700

    Ganti penamaan method pake boso londo

--- hasan/pr.rb
+++ hasan/pr.rb
@@ -10,13 +10,13 @@ arr = [1, 2, 3, 4, 5]
 end
 
 #method 1, single line method
-def kali (a, b)
+def multiply (a, b)
     puts a * b
 end
 
 #method 2, multiline method, dicampur conditional statement no 1
 #menampilkan penjumlahan dari 2 variabel, jika kurang dari 10 dan lebih akan beda outputnya
-def tambah (a, b)
+def add (a, b)
     c =  a + b
 
     if c <= 10
@@ -30,20 +30,20 @@ end
 #menampilkan value dari array berdasarkan indeksnya, sisan pakai method yang didefinisikan sebelumnya
 i = 0
 while i < arr.length
-    tambah(arr[i], num)
+    add(arr[i], num)
     i += 1
 end
 
 #iterasi no 1, menampilkan hasil perkalian
-arr.each { |val| kali(val, num) }
+arr.each { |val| multiply(val, num) }
 
 
 #iterasi no 2, conditional no 2
 arr.each do |val|
-    hasil =  num % val
-    if hasil < 3
+    result =  num % val
+    if result < 3
         puts "Sisa hasil bagi #{num} dan #{val} kurang dari 5"
     else
-        puts "Sisa hasil bagi #{num} dan #{val} adalah #{hasil}"
+        puts "Sisa hasil bagi #{num} dan #{val} adalah #{result}"
     end
 end
