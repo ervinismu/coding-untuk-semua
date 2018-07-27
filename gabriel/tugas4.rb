ac595cbbc2b3c3ec8e7988b3e7d2d537a30c70e1
Anastasia <anastasiadlarasati@gmail.com>
2018-07-27 20:34:20 +0700

    new commit

---
+++ gabriel/tugas4.rb
@@ -0,0 +1,9 @@
+#multiline
+def penjumlahan(num1, num2)
+    num = num1 + num2
+puts "hasil dari #{num1} + #{num2} = #{num}"
+end
+penjumlahan(10, 20)
+
+#single line
+def greet() return 'Hello' end
